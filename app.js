9c51f090c4058d5d128fd593c3e5b78721470c9c
jason1466 <jason1466@users.noreply.github.com>
2019-06-14 13:16:58 -0400

    init2

---
+++ app.js
@@ -0,0 +1,10 @@
+// initialize the app
+var myapp = angular.module('app', ['ngMaterial']);
+
+// set the configuration
+myapp.run(['$rootScope', function($rootScope){
+}]);
+
+// add a controller
+myapp.controller('mycontroller', ['$scope', '$http', function($scope, $http) {
+}]);
